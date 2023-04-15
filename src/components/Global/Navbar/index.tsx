9fd1ebc72052ec7ae72fdfc7996f808ab0f71a6a
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-04-15 08:59:54 +0800

    SUN/MOON Icons and Transition for background

--- src/components/Global/Navbar/index.tsx
+++ src/components/Global/Navbar/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Container } from "./styled";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -13,13 +13,26 @@ function Navbar() {
     const currentTheme = theme === "system" ? systemTheme : theme;
 
     if (currentTheme === "light") {
-      return <button onClick={() => setTheme("dark")}>dark</button>;
+      return (
+        <MoonIcon
+          className="h-6 w-6 cursor-pointer"
+          onClick={() => setTheme("dark")}
+        />
+      );
     } else {
-      return <button onClick={() => setTheme("light")}>light</button>;
+      return (
+        <SunIcon
+          className="h-6 w-6 cursor-pointer"
+          onClick={() => setTheme("light")}
+        />
+      );
     }
   };
   return (
-    <Container className="bg-second-dark">{renderThemeChanger()}</Container>
+    <Container className="bg-second-dark dark:bg-main-light  trainsition-bg">
+      <div className="bg-main-dark dark:bg-second-light h-6 w-6 transition-bg"></div>
+      <div>{renderThemeChanger()}</div>
+    </Container>
   );
 }
 
