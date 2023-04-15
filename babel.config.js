2d852ef07b971dd388fcd6b205c7491f294640e3
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-04-15 09:20:38 +0800

    Fix building issue

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   presets: ["next/babel"],
-  plugins: [["styled-components", { ssr: true }]],
+  plugins: [
+    [
+      "babel-plugin-styled-components",
+      {
+        ssr: true,
+        displayName: true,
+        preprocess: false,
+      },
+    ],
+  ],
 };
