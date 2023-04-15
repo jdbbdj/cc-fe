7a705ffa42f0de1a82a6937a9f9c0ba5b8062bed
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-04-15 08:36:56 +0800

    Toggle Dark and Light Mode Setup

--- src/components/Global/Navbar/index.tsx
+++ src/components/Global/Navbar/index.tsx
@@ -1,39 +1,25 @@
-import React from "react";
-import Typography from "../Typography";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Container } from "./styled";
 function Navbar() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "light") {
+      return <button onClick={() => setTheme("dark")}>dark</button>;
+    } else {
+      return <button onClick={() => setTheme("light")}>light</button>;
+    }
+  };
   return (
-    <div className="navbar-main h-16">
-      {/*PROMO -------------------------------------*/}
-      <div className="promo-header h-5 justify-center items-center text-center">
-        <Typography
-          type="default-white"
-          text="Super deals! Free shipping for orders above P500"
-        />
-      </div>
-
-      {/*LOGO HEADER NAVS -------------------------------------*/}
-      <div className="navbar-second flex flex-row justify-between items-center px-4">
-        <div
-          className="flex flex-row justify-between"
-          style={{ width: "calc((100% - 470px))" }}
-        >
-          <Image src="/svg/logo.svg" alt="My SVG file" width={48} height={48} />
-          <Typography type="header" text="CONG CLOTHING" />
-        </div>
-
-        <div className="navigations flex flex-row justify-around items-center w-48">
-          <Typography type="default" text="REGISTER" />
-          <Typography type="default" text="SIGN IN" />
-          <Image
-            src="/svg/shopping.svg"
-            alt="My SVG file"
-            width={24}
-            height={24}
-          />
-        </div>
-      </div>
-    </div>
+    <Container className="bg-second-dark">{renderThemeChanger()}</Container>
   );
 }
 
