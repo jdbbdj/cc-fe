14c27c5adfae92fe7589bb6912a48e77294b9b6e
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-04-18 01:58:55 +0800

    Add vido banner, Add on hover effects for products, add simple product navs

--- src/components/Home/Products/Product/index.tsx
+++ src/components/Home/Products/Product/index.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ProductItemImage,
+  ProductItemWrapper,
+  ProductItemHover,
+  ProductActions,
+} from "../styled";
+import {
+  MagnifyingGlassIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/solid";
+type Props = {
+  key: string;
+  product: {
+    title: string;
+    img: string;
+    id: number;
+  };
+};
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = (props: Props) => {
+  const [onhover, setOnHover] = useState(false);
+  const { product } = props;
+  return (
+    <ProductItemWrapper>
+      <ProductItemImage
+        alt={`Product ${product.id}`}
+        src={product.img}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className="w-full h-auto"
+      />
+      <ProductItemHover
+        onMouseOver={() => setOnHover((prev) => !prev)}
+        onMouseLeave={() => setOnHover((prev) => !prev)}
+        className={!onhover ? "none" : "block bg-black"}
+      >
+        <ProductActions
+          onMouseOver={() => setOnHover((prev) => !prev)}
+          onMouseLeave={() => setOnHover((prev) => !prev)}
+          style={{ display: !onhover ? "none" : "block", zIndex: 999 }}
+        >
+          <MagnifyingGlassIcon width={24} height={24} fill="white" />
+        </ProductActions>
+        <ProductActions
+          onMouseOver={() => setOnHover((prev) => !prev)}
+          onMouseLeave={() => setOnHover((prev) => !prev)}
+          style={{ display: !onhover ? "none" : "block", zIndex: 999 }}
+        >
+          <HeartIcon width={24} height={24} fill="white" />
+        </ProductActions>
+        <ProductActions
+          onMouseOver={() => setOnHover((prev) => !prev)}
+          onMouseLeave={() => setOnHover((prev) => !prev)}
+          style={{ display: !onhover ? "none" : "block", zIndex: 999 }}
+        >
+          <ShoppingCartIcon width={24} height={24} fill="white" />
+        </ProductActions>
+      </ProductItemHover>
+    </ProductItemWrapper>
+  );
 };
 
 export default ProductItem;
--- src/components/Home/Products/styled.ts
+++ src/components/Home/Products/styled.ts
@@ -1,12 +1,69 @@
 import styled from "styled-components";
+import Image from "next/image";
+
 export const ProductContainer = styled.section`
   width: 100%;
   height: 100vh;
   position: relative;
   display: flex;
   flex-direction: column;
+  margin-top: 10px;
+  padding-left: 25px;
+  padding-right: 25px;
 `;
 
 export const ProductSections = styled.div`
   flex: 1;
+  justify-content: center;
+  display: flex;
+  margin-top: 5px;
+`;
+
+/*PRODUCTS NAVIGATION*/
+export const ProductNavWrapper = styled.div`
+  height: 25px;
+  display: flex;
+  align-items: center;
+`;
+
+export const ProductsNav = styled.div`
+  margin-right: 50px;
+  cursor: pointer;
+  transition: opacity 0.25s ease-in-out;
+`;
+
+/*PRODUCTS PICTURES */
+
+export const ProductItemWrapper = styled.div`
+  flex: 1;
+  position: relative;
+  margin-right: 5px;
+  max-height: 227.5px;
+`;
+export const ProductItemImage = styled(Image)`
+  z-index: 25;
+`;
+
+export const ProductItemHover = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  position: absolute;
+  z-index: 26;
+  opacity: 0.4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const ProductActions = styled.div`
+  z-index: 27;
+  margin-right: 10px;
+`;
+
+/*Video */
+export const VideoWrapper = styled.div`
+  height: 45%;
+  width: 100%;
 `;
